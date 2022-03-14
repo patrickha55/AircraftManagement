1b30c692a54e246676a02b45896951bb46ef4c75
Phat <pattrickha@gmail.com>
2022-03-14 16:11:09 +0700

    added test for createAircraftModal

--- aircraft-management-client/src/pages/aircraft/__tests__/Aircraft.test.js
+++ aircraft-management-client/src/pages/aircraft/__tests__/Aircraft.test.js
@@ -1,7 +1,7 @@
+import { GraphQLError } from 'graphql';
 import { MockedProvider } from '@apollo/react-testing';
-import { cleanup, render } from '@testing-library/react';
+import { cleanup, render, waitForElementToBeRemoved } from '@testing-library/react';
 import React from 'react';
-import renderer, { act } from 'react-test-renderer';
 import Aircraft, { GET_AIRCRAFTS } from '../Aircraft';
 import wait from 'waait';
 
@@ -19,51 +19,125 @@ const aircraftMocks = [
         },
         result: {
             data: {
-                aircrafts: aircrafts
-            },
-            loading: false,
-            error: undefined
+                aircrafts
+            }
         }
     }
 ];
 
-// it("renders without error.", () => {
-//     render(
-//         <MockedProvider mocks={aircraftMocks} addTypename={false} >
-//             <Aircraft />
-//         </MockedProvider>
-//     );
-// });
+const aircraftMockWithGraphQLError = [
+    {
+        request: {
+            query: GET_AIRCRAFTS
+        },
+        result: {
+            data: {
+                aircrafts: []
+            },
+            errors: [new GraphQLError('GraphQL Error!')]
+        }
+    }
+]
 
-// it('should renders loading state initially', () => {
-//     //arrange
-//     const component = renderer.create(
-//         <MockedProvider mocks={[]}>
-//             <Aircraft />
-//         </MockedProvider>,
-//     );
+const aircraftMockWithNetworkError = [
+    {
+        request: {
+            query: GET_AIRCRAFTS
+        },
+        result: {
+            data: {
+                aircrafts: []
+            },
+            errors: [new Error('An error occurred')]
+        }
+    }
+]
 
-//     const tree = component.toJSON();
+it("renders without error.", () => {
+    render(
+        <MockedProvider mocks={aircraftMocks} addTypename={false} >
+            <Aircraft />
+        </MockedProvider>
+    );
+});
 
-//     // assert
-//     expect(tree).toMatchSnapshot();
+it('should renders loading state initially', () => {
+    //arrange
+    const { getByText } = render(
+        <MockedProvider mocks={[]} addTypename={false}>
+            <Aircraft />
+        </MockedProvider>,
+    );
 
-//     expect(tree.children).toContain('Loading...');
-// });
+    expect(getByText(/(loading)[.]+/i).textContent).toContain('Loading...');
+});
+
+it('should renders GraphQL error message', async () => {
+    const { getByRole, queryByText } = render(
+        <MockedProvider mocks={aircraftMockWithGraphQLError} addTypename={false}>
+            <Aircraft />
+        </MockedProvider>,
+    );
+
+    await waitForElementToBeRemoved(() => queryByText(/loading[.]+/i));
+
+    await wait(0);
+
+    const error = getByRole('heading');
+
+    expect(error).not.toBeNull();
+
+    expect(error.className).toMatch(/alert/i);
+
+    expect(error.textContent).toContain('GraphQL Error!');
+});
+
+it('should renders a network error message', async () => {
+    const { getByRole, queryByText } = render(
+        <MockedProvider mocks={aircraftMockWithNetworkError} addTypename={false}>
+            <Aircraft />
+        </MockedProvider>,
+    );
+
+    await waitForElementToBeRemoved(() => queryByText(/loading[.]+/i));
+
+    await wait(0);
+
+    const error = getByRole('heading');
+
+    expect(error).not.toBeNull();
+
+    expect(error.className).toMatch(/alert/i);
+
+    expect(error.textContent).toContain('An error occurred');
+});
 
 it('should renders aircrafts', async () => {
-    const component = (
+    const { findAllByText, queryByText } = render(
         <MockedProvider mocks={aircraftMocks} addTypename={false} >
             <Aircraft />
         </MockedProvider>
     );
 
+    await waitForElementToBeRemoved(() => queryByText(/loading[.]+/i));
 
-    const tree = renderer
-        .create(component)
-        .toJSON();
+    await wait(0);
 
-    await new Promise(resolve => setTimeout(resolve, 2000));
+    const manufacturers = await findAllByText(/boeing|lockheed martin/i);
+    const models = await findAllByText(/a380|f22/i);
+    const roles = await findAllByText(/wide body|superiority/i);
 
-    expect(tree).toMatchSnapshot();
+    expect(manufacturers && models && roles).not.toBeNull();
+
+    for (const manufacturer of manufacturers) {
+        expect(manufacturer.textContent).toMatch(/boeing|lockheed martin/i);
+    };
+
+    for (const model of models) {
+        expect(model.textContent).toMatch(/a380|f22/i);
+    };
+
+    for (const role of roles) {
+        expect(role.textContent).toMatch(/wide body|superiority/i);
+    }
 });
